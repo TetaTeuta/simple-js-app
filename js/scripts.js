ff351a8ee295469a9bb5c611bc56369e04c046a5
TetaTeuta <teuta.stazic@gmail.com>
2019-07-23 21:46:07 +1200

    Created list of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,29 @@
+var repository = [
 
-     alert('Hello world');
+  {name:"Bulbasaur",
+  height: 7,
+  types: ['grass', 'poison']},
+
+  {name:"Jigglypuff",
+  height: 0.5,
+  types: "Balloon"},
+
+  {name:"Snorlax",
+  height: 2.1,
+  types: "Dozing"},
+
+  {name:"Lickilicky",
+  height: 1.7,
+  types: ['Licking', 'Supersonic']},
+
+];
+
+
+for (var i=0; i<repository.length;i++){
+  document.write(repository[i].name);
+  document.write(repository[i].height);
+
+  if (repository[i].height > 5){
+    document.write(' - Wow, that\’s big!');
+}
+}
