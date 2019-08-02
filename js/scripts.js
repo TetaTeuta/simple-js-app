7ba7296bea028887fde7426e96f6e53e27386bda
TetaTeuta <teuta.stazic@gmail.com>
2019-08-02 18:47:09 +1200

    Added buttons

--- js/scripts.js
+++ js/scripts.js
@@ -33,24 +33,38 @@ return {
   add: add,
   getAll: getAll
 
-};
+}
+
+function addListItem (pokemon){
+
+}
+
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon){ document.write('<p>'+ pokemon.name+'</p>')})
+var $pokemonList = document.querySelector('.pokemon-list');   //create a variable above the forEach loop block, then assign it the ul element you just added to your “index.html” file
+
+//forEach loop
+pokemonRepository.getAll().forEach(function (list){
+
+  var $listItem = document.createElement('li');   //create an li element that contains a button for each Pokémon
+  var $pokemonDetailsButton = document.createElement('button');
+  $pokemonDetailsButton.innerText = pokemon.name;                      //Set the innerText of the button to be the Pokémon's name
+  $pokemonDetailsButton.classList.add('button__style');   //Add a class to the button using the classList.add method
+  $listItem.appendChild($pokemonDetailsButton);      //append the button to the list item as its child.
+  $listItem.classList.add('pokemon-list__item');
+  $pokemonList.appendChild($listItem);           //append the list item to the unordered list as its child.
+
+
+});
 
 
 
 
 
-// function loopBlockFunction(currentName){
-//   document.write("<h3> " + currentName.name + "</h3>");
-//   document.write("<p>" + 'Type: ' + currentName.types + "</p>")
-//   document.write("<p>" + 'Height: ' + currentName.height + "</p>")
-//
-//
-//   if (currentName.height > 5){
-//     document.write("<p>" + ' - Wow, that\’s big!' + "</p>");
-//
-//   }
-// }
-// pokemonRepository.forEach(loopBlockFunction);
+
+
+
+// var $title = document.querySelector('.title');
+// var button = document.createElement('button');
+// button.innerText = "Pokemon app";
+// $title.appendChild(button);
